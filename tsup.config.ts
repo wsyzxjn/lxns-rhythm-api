72d33c69fcec1aa9fdc0d170b92d0b52c2ca750d
Amatsuka <45129539+wsyzxjn@users.noreply.github.com>
2025-09-19 03:58:04 +0800

    📦 build(package): 更新构建配置
    - 在 package.json 中添加 types 导出和修正路径，以支持 TypeScript 类型定义
    - 重构 tsup.config.ts，将配置分离为 ESM 和 CJS，提升构建系统灵活性和模块支持

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,14 +1,25 @@
 // tsup.config.ts
-import { defineConfig } from "tsup";
+import { defineConfig, type Options } from "tsup";
 
 const isProduction = process.env.NODE_ENV === "production";
 
-export default defineConfig({
+const DEFAULT_CONFIG: Options = {
   entry: ["src/index.ts"],
-  format: ["esm", "cjs"],
   dts: true,
   minify: isProduction,
   clean: true,
   outDir: "dist",
   skipNodeModulesBundle: true,
-});
+};
+
+export default defineConfig([
+  {
+    ...DEFAULT_CONFIG,
+    format: "esm",
+  },
+  {
+    ...DEFAULT_CONFIG,
+    format: "cjs",
+    dts: false,
+  },
+]);
